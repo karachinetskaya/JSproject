55090f267669d11cb5deb130027f19727fecedea
Lena <karachinetskaya.lena@gmail.com>
2021-12-08 00:12:49 +0200

    Add loops(second practice)

--- script2.js
+++ script2.js
@@ -8,7 +8,25 @@ const personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
+let i = 0;
+
+// while (i < 2) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените фильм?", "");
+//   console.log(a);
+//   console.log(b);
+
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("ok!");
+//     i++;
+//   } else {
+//     console.log("Error");
+//     i--;
+//   }
+// }
+
+do {
   const a = prompt("Один из последних просмотренных фильмов?", ""),
     b = prompt("На сколько оцените фильм?", "");
   console.log(a);
@@ -17,11 +35,27 @@ for (let i = 0; i < 2; i++) {
   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
     personalMovieDB.movies[a] = b;
     console.log("ok!");
+    i++;
   } else {
     console.log("Error");
     i--;
   }
-}
+} while (i < 2);
+
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените фильм?", "");
+//   console.log(a);
+//   console.log(b);
+
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("ok!");
+//   } else {
+//     console.log("Error");
+//     i--;
+//   }
+// }
 
 if (personalMovieDB.count < 10) {
   console.log("Просмотренно довольно мало фильмов");
